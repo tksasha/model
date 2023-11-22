b4206c481d8f2e5ed114f3bbcbdb65ed70699cf2
Oleksandr Tkachuk <tksasha.me@gmail.com>
2023-11-22 13:54:29 +0200

    removed unused

--- model.go
+++ model.go
@@ -1,12 +1,7 @@
 package model
 
-import (
-	"database/sql"
-)
-
 type Model struct {
-	DB     *sql.DB `json:"-"`
-	Errors Errors  `json:"-"`
+	Errors Errors `json:"-"`
 }
 
 func (model *Model) IsValid() bool {
